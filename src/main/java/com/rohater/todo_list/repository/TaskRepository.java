dc3a44aa6e2f2c40fc249dbc24e25385c83bf83a
RohatErgun <rohatergun06@gmail.com>
2026-04-04 17:51:53 +0200

    Add custom CRUD methods to TaskRepository findTaskById, findTasksByCreationDateBetween, findAllByCreationDate

--- src/main/java/com/rohater/todo_list/repository/TaskRepository.java
+++ src/main/java/com/rohater/todo_list/repository/TaskRepository.java
@@ -3,8 +3,16 @@ package com.rohater.todo_list.repository;
 import com.rohater.todo_list.model.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TaskRepository
         extends CrudRepository<Task, Long>{
 
+   Task findTaskById(Long id);
 
+   List<Task> findTasksByCreationDateBetween(LocalDateTime start, LocalDateTime end);
+
+   List<Task> findAllByCreationDate(LocalDateTime creationDate);
 }

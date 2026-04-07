98e5bba8c9896aafeede1401c04f2919b85211bd
RohatErgun <rohatergun06@gmail.com>
2026-04-07 09:13:15 +0200

    Add TaskService.java

--- src/main/java/com/rohater/todo_list/service/TaskService.java
+++ src/main/java/com/rohater/todo_list/service/TaskService.java
@@ -1,2 +1,37 @@
-package com.rohater.todo_list.service;public class TaskService {
+package com.rohater.todo_list.service;
+
+import com.rohater.todo_list.model.Task;
+import com.rohater.todo_list.repository.TaskRepository;
+import com.rohater.todo_list.model.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepo;
+
+    public TaskService(TaskRepository taskRepo){
+        this.taskRepo = taskRepo;
+    }
+
+    public Task findTaskById(Long id){
+
+    }
+
+    public List<Task> findTasksByCreationDateBetween(LocalDateTime start, LocalDateTime end){
+
+    }
+
+    public List<Task> findAllByCreationDate(LocalDateTime creationDate){
+
+    }
+
+    public List<Task> findByUser(User user){
+
+    }
+
 }
